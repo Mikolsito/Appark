b49a2bea9cc22c4a0e750f684430bc178ba63867
IceFox72 <pferreme7@alumnes.ub.edu>
2022-05-04 23:04:08 +0200

    Funcions de Register i Login

--- app/src/main/java/com/example/appark/Activities/LoginActivity.java
+++ app/src/main/java/com/example/appark/Activities/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.appark.R;
 
@@ -26,11 +27,22 @@ public class LoginActivity extends Activity {
         mail = (EditText) findViewById(R.id.et_email);
         contrasenya = (EditText) findViewById(R.id.et_password);
 
+        //TODO crear una classe DataBaseHelper per a gestionar la base de dades
+        //databaseHelper = new DatabaseHelper(this);
+
         entra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent processar_main = new Intent(view.getContext(), MainActivity.class);
-                startActivityForResult(processar_main, 0);
+                String username = mail.getText().toString();
+                String password = contrasenya.getText().toString();
+
+                if(true){ // if (databaseHelper.CheckLogin(username, password)) {
+                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                    Intent processar_main = new Intent(view.getContext(), MainActivity.class);
+                    startActivityForResult(processar_main, 0);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Correu o contrasenya invalids", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
